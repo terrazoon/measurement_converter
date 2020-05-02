feb9276b770f1c2538fbc2f90446e5e50ee64040
Kenneth Kehl <k3nn3th.k3hl@gmail.com>
2020-05-02 14:37:03 -0700

    a little test cleanup

--- tests/services/temperature_service_test.py
+++ tests/services/temperature_service_test.py
@@ -1,36 +1,39 @@
 import unittest
 
 from src.services.temperature_service import TemperatureService
+from src.services.validation_constants import ValidationConstants
 
 
 class TemperatureServiceTest(unittest.TestCase):
     test_list = [
-        [32, 0.0000, 'f', 'c', 'correct'],
-        [0.0000, 32, 'c', 'f', 'correct'],
-        [0, 459.67, 'f', 'r', 'correct'],
-        [459.67, 0, 'r', 'f', 'correct'],
-        [32, 273.15, 'f', 'k', 'correct'],
-        [273.15, 32, 'k', 'f', 'correct'],
-        [84.2, 543.94, 'f', 'r', 'correct'],
-        [32, 1000.1, 'f', 'c', 'incorrect'],
-        [6.5, 'dog', 'f', 'r', 'incorrect'],
-        [32, 1000, 'f', 'z', 'invalid'],
-        [32, 32, 'f', 'f', 'correct'],
-        [136.1, 45.32, 'dog', 'c', 'invalid'],
-        [317.33, 111.554, 'k', 'f', 'incorrect'],
-        [900, 500, 'r', 'k', 'correct'],
-        [0, 273.15, 'c', 'k', 'correct'],
-        [273.15, 0, 'k', 'c', 'correct'],
-        [1, 2, 'f', 'f', 'incorrect'],
-        [0, 491.67, 'c', 'r', 'correct'],
-        [491.67, 0, 'r', 'c', 'correct'],
-        [500, 900, 'k', 'r', 'correct'],
-        ['dog', 24, 'k', 'r', 'invalid'],
-        [None, 24, 'k', 'r', 'invalid']
+        [32, 0.0000, 'f', 'c', ValidationConstants.CORRECT],
+        [0.0000, 32, 'c', 'f', ValidationConstants.CORRECT],
+        [0, 459.67, 'f', 'r', ValidationConstants.CORRECT],
+        [459.67, 0, 'r', 'f', ValidationConstants.CORRECT],
+        [32, 273.15, 'f', 'k', ValidationConstants.CORRECT],
+        [273.15, 32, 'k', 'f', ValidationConstants.CORRECT],
+        [84.2, 543.94, 'f', 'r', ValidationConstants.CORRECT],
+        [32, 1000.1, 'f', 'c', ValidationConstants.INCORRECT],
+        [6.5, 'dog', 'f', 'r', ValidationConstants.INCORRECT],
+        [32, 1000, 'f', 'z', ValidationConstants.INVALID],
+        [32, 32, 'f', 'f', ValidationConstants.CORRECT],
+        [136.1, 45.32, 'dog', 'c', ValidationConstants.INVALID],
+        [317.33, 111.554, 'k', 'f', ValidationConstants.INCORRECT],
+        [900, 500, 'r', 'k', ValidationConstants.CORRECT],
+        [0, 273.15, 'c', 'k', ValidationConstants.CORRECT],
+        [273.15, 0, 'k', 'c', ValidationConstants.CORRECT],
+        [1, 2, 'f', 'f', ValidationConstants.INCORRECT],
+        [0, 491.67, 'c', 'r', ValidationConstants.CORRECT],
+        [491.67, 0, 'r', 'c', ValidationConstants.CORRECT],
+        [500, 900, 'k', 'r', ValidationConstants.CORRECT],
+        ['dog', 24, 'k', 'r', ValidationConstants.INVALID],
+        [None, 24, 'k', 'r', ValidationConstants.INVALID]
 
     ]
 
     def test_temperature_conversions(self):
         for test in self.test_list:
             result = TemperatureService.get_converted_temperature(test[0], test[1], test[2], test[3])
+            if result != test[4]:
+                print(f" {result} {test}")
             assert result == test[4]
--- tests/services/volume_service_test.py
+++ tests/services/volume_service_test.py
@@ -1,48 +1,49 @@
 import unittest
 
+from src.services.validation_constants import ValidationConstants
 from src.services.volume_service import VolumeService
 
 test_list = [
-    [25.6, 6.1, 'c', 'l', 'correct'],
-    [6.0567, 25.6, 'l', 'c', 'correct'],
-    [16, 1, 'c', 'g', 'correct'],
-    [1, 16, 'g', 'c', 'correct'],
-    [1, 1728, 'f', 'i', 'correct'],
-    [1728, 1, 'i', 'f', 'correct'],
-    [1, 16, 'c', 't', 'correct'],
-    [16, 1, 't', 'c', 'correct'],
-    [1, 119.7, 'f', 'c', 'correct'],
-    [119.7, 1, 'c', 'f', 'correct'],
-    [14.438, 1, 'i', 'c', 'correct'],
-    [1, 14.438, 'c', 'i', 'correct'],
-    [1, 256, 'g', 't', 'correct'],
-    [256, 1, 't', 'g', 'correct'],
-    [1, 1915.01, 'f', 't', 'correct'],
-    [1915.01, 1, 't', 'f', 'correct'],
-    [1, 1.10823, 'i', 't', 'correct'],
-    [1.10823, 1, 't', 'i', 'correct'],
-    [7.48052, 1, 'g', 'f', 'correct'],
-    [1, 7.48052, 'f', 'g', 'correct'],
-    [1, 231, 'g', 'i', 'correct'],
-    [231, 1, 'i', 'g', 'correct'],
-    [4, 4, 'i', 'g', 'incorrect'],
-    [1, 12, 'f', 't', 'incorrect'],
-    [1, 2, 'y', 'z', 'invalid'],
-    ['dog', 1, 'f', 'i', 'invalid'],
-    [1, 'dog', 'f', 'i', 'incorrect'],
-    ['1', '231', 'g', 'i', 'correct'],
-    [1, 67.628, 'l', 't', 'correct'],
-    [67.628, 1, 't', 'l', 'correct'],
-    [1, 61.028, 'l', 'i', 'correct'],
-    [61.028, 1, 'i', 'l', 'correct'],
-    [1, 0.0353147, 'l', 'f', 'correct'],
-    [0.0353147, 1, 'f', 'l', 'correct'],
-    [1, 1, 'f', 'f', 'correct'],
-    [1, 2, 'f', 'f', 'incorrect'],
-    [1, 1, 'f', 'z', 'invalid'],
-    [1, 1, 'cubic-feet', 'cubic-feet', 'correct'],
-    [1, 1, 'cubic-inches', 'cubic-inches', 'correct'],
-    [73.12, 19.4, 'gallons', 'kelvin', 'invalid']
+    [25.6, 6.1, 'c', 'l', ValidationConstants.CORRECT],
+    [6.0567, 25.6, 'l', 'c', ValidationConstants.CORRECT],
+    [16, 1, 'c', 'g', ValidationConstants.CORRECT],
+    [1, 16, 'g', 'c', ValidationConstants.CORRECT],
+    [1, 1728, 'f', 'i', ValidationConstants.CORRECT],
+    [1728, 1, 'i', 'f', ValidationConstants.CORRECT],
+    [1, 16, 'c', 't', ValidationConstants.CORRECT],
+    [16, 1, 't', 'c', ValidationConstants.CORRECT],
+    [1, 119.7, 'f', 'c', ValidationConstants.CORRECT],
+    [119.7, 1, 'c', 'f', ValidationConstants.CORRECT],
+    [14.438, 1, 'i', 'c', ValidationConstants.CORRECT],
+    [1, 14.438, 'c', 'i', ValidationConstants.CORRECT],
+    [1, 256, 'g', 't', ValidationConstants.CORRECT],
+    [256, 1, 't', 'g', ValidationConstants.CORRECT],
+    [1, 1915.01, 'f', 't', ValidationConstants.CORRECT],
+    [1915.01, 1, 't', 'f', ValidationConstants.CORRECT],
+    [1, 1.10823, 'i', 't', ValidationConstants.CORRECT],
+    [1.10823, 1, 't', 'i', ValidationConstants.CORRECT],
+    [7.48052, 1, 'g', 'f', ValidationConstants.CORRECT],
+    [1, 7.48052, 'f', 'g', ValidationConstants.CORRECT],
+    [1, 231, 'g', 'i', ValidationConstants.CORRECT],
+    [231, 1, 'i', 'g', ValidationConstants.CORRECT],
+    [4, 4, 'i', 'g', ValidationConstants.INCORRECT],
+    [1, 12, 'f', 't', ValidationConstants.INCORRECT],
+    [1, 2, 'y', 'z', ValidationConstants.INVALID],
+    ['dog', 1, 'f', 'i', ValidationConstants.INVALID],
+    [1, 'dog', 'f', 'i', ValidationConstants.INCORRECT],
+    ['1', '231', 'g', 'i', ValidationConstants.CORRECT],
+    [1, 67.628, 'l', 't', ValidationConstants.CORRECT],
+    [67.628, 1, 't', 'l', ValidationConstants.CORRECT],
+    [1, 61.028, 'l', 'i', ValidationConstants.CORRECT],
+    [61.028, 1, 'i', 'l', ValidationConstants.CORRECT],
+    [1, 0.0353147, 'l', 'f', ValidationConstants.CORRECT],
+    [0.0353147, 1, 'f', 'l', ValidationConstants.CORRECT],
+    [1, 1, 'f', 'f', ValidationConstants.CORRECT],
+    [1, 2, 'f', 'f', ValidationConstants.INCORRECT],
+    [1, 1, 'f', 'z', ValidationConstants.INVALID],
+    [1, 1, 'cubic-feet', 'cubic-feet', ValidationConstants.CORRECT],
+    [1, 1, 'cubic-inches', 'cubic-inches', ValidationConstants.CORRECT],
+    [73.12, 19.4, 'gallons', 'kelvin', ValidationConstants.INVALID]
 ]
 
 
@@ -51,4 +52,6 @@ class VolumeServiceTest(unittest.TestCase):
     def test_volume_conversions(self):
         for test in test_list:
             result = VolumeService.get_converted_volume(test[0], test[1], test[2], test[3])
+            if result != test[4]:
+                print(f"{result} {test}")
             assert result == test[4]
